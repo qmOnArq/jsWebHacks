0e1de436f8fe6841f61c524c17f09f6e287cec95
mOnAr <qmonarq@gmail.com>
2019-02-20 15:50:07 +0100

    localization_highlighter - use mutation observer

--- scripts/localization_highlighter.js
+++ scripts/localization_highlighter.js
@@ -1,20 +1,24 @@
-setInterval(
-    () => {
-        $('*').each(function(){ 
-            const text = $(this)
-                .clone()
-                .children()
-                .remove()
-                .end()
-                .text()
-                .trim(); 
-            const hasNumber = !!(' ' + text).match(/[^\u200B\d](\d+.?\d*)+/g);
-            
-            if (hasNumber) {
-                $(this).css({background: 'red', color: 'white'});
-            }
-        });
-    }
-, 1000);
+const config = {
+    subtree: true,
+    characterData: true,
+    childList: false,
+    attributes: false,
+};
+
+const observer = new MutationObserver(handle);
+
+observer.observe(document.body, config);
+
+function handle(mutationsList) {
+    mutationsList.forEach(mutation => {
+        const node = mutation.target;
+        const text = node.nodeValue;
+        if (!!(' ' + text).match(/[^\u200B\d](\d+.?\d*)+/g)) {
+            const style = node.parentElement.style;
+            style.backgroundColor = 'red';
+            style.color = 'white';
+        }
+    });
+}
 
 // Uses \u200B to mark localized entities
