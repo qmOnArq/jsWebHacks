31a78d7bb338f1d6a1d581bdc98d509923422ed8
František Gič <mcfreddie777@gmail.com>
2021-03-19 12:04:20 +0100

    gitlab - refactor hashing and parsing parameters using URLSearchParams

--- src/gitlab/functions/hash-variables.ts
+++ src/gitlab/functions/hash-variables.ts
@@ -1,29 +1,24 @@
-export function getHashVariables() {
+import { KeyValuePair } from '../../types/name-value-pair.type';
+
+export function getHashVariables(): KeyValuePair {
     let hashString = location.hash;
+
     if (hashString[0] === '#') {
         hashString = hashString.substr(1, hashString.length);
     }
 
-    const result: Record<string, string> = {};
+    const variables: KeyValuePair = {};
 
-    const parts = hashString.split('~&~');
-    parts.forEach(part => {
-        const split = part.split('~=~');
-        if (!split[0]) {
-            return;
-        }
-        result[split[0]] = split[1];
+    new URLSearchParams(hashString).forEach((value, key) => {
+        variables[key] = value;
     });
 
-    return result;
+    return variables;
 }
 
-export function createHashString(values: Record<string, string>) {
-    let result = '';
-
-    Object.keys(values).forEach(key => {
-        result += key + '~=~' + values[key] + '~&~';
-    });
-
-    return result ? '#' + result : '';
+export function createHashString(values: KeyValuePair) {
+    if (!Object.values(values).length) {
+        return '';
+    }
+    return `#${new URLSearchParams(values).toString()}`;
 }
